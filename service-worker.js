cc18aae19ec95258179b6fed666aa5190f0a9d1c
Daisuke Motohashi <ddssk.m@gmail.com>
2024-12-01 07:30:02 +0900

    古いキャッシュを削除する

--- service-worker.js
+++ service-worker.js
@@ -20,3 +20,21 @@ self.addEventListener("fetch", (event) => {
             .then((response) => response || fetch(event.request))
     );
 });
+
+// activateイベントリスナーを登録
+self.addEventListener("activate", (event) => {
+    // 古いキャッシュを削除するための非同期処理を待つ
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            // すべてのキャッシュ名を取得し、Promise.allで非同期削除を実行
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    // 現在のキャッシュ名と異なる場合は削除
+                    if (cacheName !== currentCacheName) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
